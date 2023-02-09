f75ace4b87fa2e4a850c8e72d785aa712794dc10
dnriski <nugrohoriski159@gmail.com>
2023-02-09 18:09:31 +0700

    tugas7

---
+++ tugas7.js
@@ -0,0 +1,8 @@
+var tinggiBadan = [173, 287, 123, 156, 167, 180, 175, 176, 177, 187];
+for (i = 0; i < tinggiBadan.length; i++) {
+  console.log(tinggiBadan[i]);
+}
+
+for (let tinggi of tinggiBadan) {
+  console.log(tinggi);
+}
